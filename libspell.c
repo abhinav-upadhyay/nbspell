76572f9aeb17ec1c8ff43db65c04c4a4a4144c1d
Abhinav Upadhyay <abhinav@NetBSD.org>
2017-01-18 02:41:15 +0530

    Allocate memory for char **, not char *. Also, don't overrun the array.

--- libspell.c
+++ libspell.c
@@ -293,16 +293,14 @@ spell_get_corrections(spell_t *spell, word_list *candidate_list, size_t n, int n
 		return NULL;
 	}
 
-	char **corrections = emalloc((n + 1) * sizeof(char *));
-	corrections[n] = NULL;
+	size_t arraysize = n < corrections_count? n: corrections_count;
+	char **corrections = emalloc((arraysize + 1) * sizeof(*corrections));
+	corrections[arraysize] = NULL;
 	qsort(wc_array, corrections_count, sizeof(*wc_array), max_count);
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < arraysize; i++) {
 		if (wc_array[i].word)
 			corrections[i] =  estrdup(wc_array[i].word);
 	}
-	//XXX: Handle the case when n < corrections_count
-	if (corrections_count < n)
-		corrections[corrections_count] = NULL;
 	free(wc_array);
 	return corrections;
 }
@@ -489,12 +487,17 @@ spell_get_suggestions(spell_t *spell, char *word, int ngram)
 	word_list *candidates2;
 	lower(word);
 	candidates = edits1(word);
-	corrections = spell_get_corrections(spell, candidates, 1, ngram);
-	if (corrections == NULL) {
+	word_list *tail = candidates;
+	candidates2 = edits2(candidates);
+	while(tail->next)
+		tail = tail->next;
+	tail->next= candidates2;
+	corrections = spell_get_corrections(spell, candidates, 5, ngram);
+/*	if (corrections == NULL) {
 		candidates2 = edits2(candidates);
-		corrections = spell_get_corrections(spell, candidates2, 1, ngram);
+		corrections = spell_get_corrections(spell, candidates2, 5, ngram);
 		free_word_list(candidates2);
-	}
+	}*/
 	free_word_list(candidates);
 	return corrections;
 }
